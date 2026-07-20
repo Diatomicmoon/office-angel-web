af6f1eab43f3bf102ea3d0ba848dcc2f6e63c085
Diatomicmoon <Diatomicmoon@users.noreply.github.com>
2026-07-19 20:58:22 -0500

    fix(layout): force load leaflet css to prevent gray map bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="theme-color" content="#000000" />
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossOrigin="" />
       </head>
       <body className={`${inter.className} bg-gray-50 text-gray-900`}>
         <div className="flex h-[100dvh] overflow-hidden">
